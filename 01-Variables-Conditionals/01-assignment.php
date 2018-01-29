07b2c99263ce9e5dac8c862affd1073bf290062d
James Meyer <jim@c130games.com>
2018-01-28 18:40:12 -0600

    Add files via upload
    Tightened up code a bit

--- 01-Variables-Conditionals/01-assignment.php
+++ 01-Variables-Conditionals/01-assignment.php
@@ -62,16 +62,19 @@
     
 <?php
 
-    $date1 = new DateTime("2018-01-28"); //can't figure out how to use current date
+    $date1 = new DateTime("2018-01-28"); //can't figure out how to use current date with DateTime function
     $date2 = new DateTime("2018-04-27");
     $diff = $date2->diff($date1)->format("%a");
+    
     echo "$diff days until my birthday.";
 ?>
+    
 <br/>
+    
 <?php
     $now = time(); // or your date as well
-    $your_date = strtotime("2018-04-27");
-    $datediff = $your_date - $now;
+    $birthdayDate = strtotime("2018-04-27");
+    $datediff = $birthdayDate - $now;
 
     echo round($datediff / (60 * 60 * 24)) . " days until my birthday.";
 ?>
@@ -85,11 +88,11 @@
 $time = date("H");
 
 if ($time < "12") {
-    echo "Good morning!";
+    echo "Morning!";
 } elseif ($time < "18") {
-    echo "Have a good day!";
+    echo "Afternoon!";
 } else {
-    echo "Have a good night!";
+    echo "Night!";
 }
 ?>
 
@@ -97,7 +100,7 @@ if ($time < "12") {
 <br/>
 <!--06. Echo to page-->
 
-
+<!-- Echo used throughout file -->
 
 
 
